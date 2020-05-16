e86519963be02ea16632ce486eafbf8eeca5a57c
Shaurya Madan <shauryamadan@Shauryas-MacBook-Pro.local>
2020-05-16 17:08:26 -0400

    Updated resume and work history details

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -20,12 +20,26 @@ export function ExperienceSection() {
                     </Col>
                     <Col lg="8">
                         <h2 className="employer-name">SS&C Technologies</h2>
-                        <h5 className="position-and-time">Fullstack Developer | January - May 2020</h5>
+                        <h5 className="position-and-time">Fullstack Developer | January - April 2020</h5>
                         <p>
-                            I used Django to maintain and update web pages and transactions for the Avenza MapStore, in addition to creating and updating models
-                            for the MapStore’s PostgreSQL database with the Django Queryset API. I also created a tool to extract GIS metadata from maps using
-                            the GDAL C++ library, and another one to populate test databases which streamlined unit testing and development. The web site was
-                            deployed with Heroku, and development was done on VMs built with Vagrant.
+                            I engineered the search functionality for the new SS&C KnowledgeBase, implementing it end-to-end with Angular, Spring Boot, and PostgreSQL's
+                            tsvector tool. It was designed to be fast, meet client reliability standards, and customized to work with terminology used in mutual fund
+                            management through the use of database dictionaries.
+                            <br></br>
+                            <br></br>
+                            I designed the workflow process that would be used by site admins to handle recommendations and create and edit documents, with several built-in
+                            checkpoints to ensure that only high quality documents made it through review. I developed the REST APIs and UI to support this process with
+                            Spring Boot and Angular, respectively.
+                            <br></br>
+                            <br></br>
+                            I was involved in the design and creation of several pages on the KnowledgeBase that were critical to its core functionality, done with Angular
+                            and Angular Material.
+                            <br></br>
+                            <br></br>
+                            I collaborated with team members to architect a database schema for search and the recommendation workflow.
+                            <br></br>
+                            <br></br>
+                            I implemented a client-side authorization framework to ensure confidential documents could only be viewed by those with appropriate permissions.
                         </p>
                     </Col>
                 </Row>
@@ -39,10 +53,19 @@ export function ExperienceSection() {
                         <h2 className="employer-name">Avenza Systems Inc</h2>
                         <h5 className="position-and-time">Software Developer (Python) | May - August 2019</h5>
                         <p>
-                            I used Django to maintain and update web pages and transactions for the Avenza MapStore, in addition to creating and updating models
-                            for the MapStore’s PostgreSQL database with the Django Queryset API. I also created a tool to extract GIS metadata from maps using
-                            the GDAL C++ library, and another one to populate test databases which streamlined unit testing and development. The web site was
-                            deployed with Heroku, and development was done on VMs built with Vagrant.
+                            I developed an algorithm with Django's QuerySet API that would populate test databases according to to business logic, with minimal additional
+                            configuration, thereby reducing the time spent on writing test fixtures for unit tests and allowing developers to focus on creating new features.
+                            <br></br>
+                            <br></br>
+                            I created a tool using the GDAL C++ library that would extract GIS metadata from maps on the Avenza MapStore and compute values such as area and
+                            map resolution, which would then be used for categorizing and filtering products in the database.
+                            <br></br>
+                            <br></br>
+                            I enhanced an existing status monitoring program to be more granular, allowing site admins to quickly perform root cause analysis during system
+                            outages.
+                            <br></br>
+                            <br></br>
+                            I implemented new features and fixed various bugs on the MapStore, improving user experience.
                         </p>
                     </Col>
                 </Row>
@@ -53,12 +76,12 @@ export function ExperienceSection() {
                         </div>
                     </Col>
                     <Col lg="8">
-                        <h2 className="employer-name">Open Text Corporation</h2>
+                        <h2 className="employer-name">OpenText Corporation</h2>
                         <h5 className="position-and-time">Software Developer, Extream Product | September - December 2018</h5>
                         <p>
-                            I expanded the Exstream PYFT framework built on top of pywinauto, and used it to develop Python scripts that automated regression testing
-                            of the OpenText Exstream software suite. I wrote many unit tests for my code that extensively used Python’s built-in testing frameworks,
-                            and added all this to the OpenText codebase using Subversion.
+                            I expanded the OpenText PyFT framework built on top of pywinauto, improving the breadth and quality of regression tests that could be made with
+                            it. I used PyFT to automate over 200 manual regression tests, accelerating the rate at which new builds of the Exstream product suite could be
+                            verified. I also improved existing regression tests to make them run as much as 5x as fast while maintaining a 100% success rate for detecting bugs.
                         </p>
                     </Col>
                 </Row>
