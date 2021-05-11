911e861c892de7529275d25c0b3e3872f6c123bb
ShauryaMdn <shaurya8990@gmail.com>
2021-05-10 20:01:56 -0400

    Added fade in effect on load

--- src/components/shaurya-information.tsx
+++ src/components/shaurya-information.tsx
@@ -1,11 +1,19 @@
-import React from  'react';
+import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
 
 import './component-styles.scss'
 import { Row, Container } from 'reactstrap';
 
 export function ShauryaInformation() {
+    let hiddenClass;
+    let setHiddenClass: Dispatch<SetStateAction<string>>;
+    [hiddenClass, setHiddenClass] = useState("info-hidden");
+
+    useEffect(() => {
+        return setHiddenClass("");
+    })
+
     return (
-        <Container fluid className="info-container" id="information">
+        <Container fluid className={`info-container ${hiddenClass}`} id="information">
             <div className="name-container">
                 <Row className="justify-content-center name">Shaurya</Row>
                 <Row className="justify-content-center name">Madan</Row>
